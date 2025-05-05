37fd89173a5a1457e6c7a6e42d7603e9f388c1a2
kamranikramofficial <kamranikram706@gmail.com>
2025-05-05 06:35:13 -0700

    add day 1

---
+++ Day-01/index.php
@@ -0,0 +1,56 @@
+
+<?php 
+    // PHP key point 
+         // In PHP, keywords (e.g. if, else, while, echo, etc.), classes, functions, and user-defined functions are not case-sensitive
+         // but However; all variable names are case-sensitive!
+
+    // Basic PHP Syntax 
+       //   this is also use direct  in html than use this 
+       //  <?php 
+
+    // Basic output tag 
+        #echo is tha tag whic 
+       //  echo "Hello World!"; 
+    
+
+    # Comment in php
+        # There are three type of comment in php
+        // This is a single-line comment
+        # This is also a single-line comment
+        /* This is a
+        multi-line comment */
+
+
+    // Creating (Declaring) PHP Variables
+        // When you assign a text value to a variable, put quotes around the value.
+        // Unlike other programming languages, PHP has no command for declaring a variable. It is created the moment you first assign a value to it.
+ 
+
+    // Rules for PHP variables:
+
+       // A variable starts with the $ sign, followed by the name of the variable
+       // A variable name must start with a letter or the underscore character
+       // A variable name cannot start with a number
+       // A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+       // Variable names are case-sensitive ($age and $AGE are two different variables)
+    // Example
+        // $txt = "W3Schools.com";
+        // echo "I love " . $txt . "!"; 
+  
+    // PHP supports the following data types:
+        // String
+        // Integer
+        // Float (floating point numbers - also called double)
+        // Boolean
+        // Array
+        // Object
+        // NULL
+        // Resource
+    
+    // Get the Type
+         // To get the data type of a variable, use the var_dump() function.
+    
+    
+     //  PHP Variables Scope
+
+ ?>
